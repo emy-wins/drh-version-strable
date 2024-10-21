909014b6fbfaac4f3e79203bea8c44324e85252c
emy-wins <benkhadra.iman@gmail.com>
2024-10-21 15:46:13 +0200

    #update -add sections of nav menu

--- src/components/accueil/accueil.js
+++ src/components/accueil/accueil.js
@@ -1,7 +1,9 @@
 $(document).ready(function ($) {
+  $(".loader").delay(2000).fadeOut("slow");
   if ($(".menu").length) {
     $(".menu li").each(function () {
       $(this).click(function () {
+        $(".loader").show();
         let display = $(this).data('display');
         $('.menu li').removeClass('active-itm');
         $(this).addClass('active-itm');
@@ -12,7 +14,22 @@ $(document).ready(function ($) {
             $(this).addClass('item-show');
           }
         });
+        $(".loader").delay(1000).fadeOut("slow");
       });
+    });
+  }
+  if ($(".menu").length) {
+    $(".slider-eq").slick({
+      slidesToShow: 3,
+      infinite: true,
+      slidesToScroll: 1,
+      dots: false,
+      arrows: false,
+      speed: 5000,
+      autoplay: true,
+      autoplaySpeed: 0,
+      cssEase: 'linear',
+      variableWidth: true,
     });
   }
 });
