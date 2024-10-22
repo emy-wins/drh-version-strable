229c6a0d661210f78ca013f89ac6f88fcc816320
emy-wins <benkhadra.iman@gmail.com>
2024-10-22 14:47:32 +0200

    #update -update login form

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -2,3 +2,25 @@ AOS.init({
   once: true,
   mirror: false,
 });
+$(document).ready(function ($) {
+  if ($(".form-login").length) {
+    $('.btn-oblier').click(function () {
+      $('.form-login').each(function () {
+        $(this).removeClass('show-form');
+      });
+      $('.form-verifier').addClass('show-form');
+    });
+    $('.btn-verifier').click(function () {
+      $('.form-login').each(function () {
+        $(this).removeClass('show-form');
+      });
+      $('.form-modifier').addClass('show-form');
+    });
+    $('.btn-modifier').click(function () {
+      $('.form-login').each(function () {
+        $(this).removeClass('show-form');
+      });
+      $('.lg-form').addClass('show-form');
+    });
+  }
+});
