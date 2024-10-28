eb931f28b3d1252d681ba5fc146210752eb1b965
emy.wins <benkhadra.iman@gmail.com>
2024-10-28 12:12:20 +0100

    update

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -5,22 +5,28 @@ AOS.init({
 $(document).ready(function ($) {
   if ($(".form-login").length) {
     $('.btn-oblier').click(function () {
+      $(".loader").show();
       $('.form-login').each(function () {
         $(this).removeClass('show-form');
       });
       $('.form-verifier').addClass('show-form');
+      $(".loader").delay(1000).fadeOut("slow");
     });
     $('.btn-verifier').click(function () {
+      $(".loader").show();
       $('.form-login').each(function () {
         $(this).removeClass('show-form');
       });
       $('.form-modifier').addClass('show-form');
+      $(".loader").delay(1000).fadeOut("slow");
     });
     $('.btn-modifier').click(function () {
+      $(".loader").show();
       $('.form-login').each(function () {
         $(this).removeClass('show-form');
       });
       $('.lg-form').addClass('show-form');
+      $(".loader").delay(1000).fadeOut("slow");
     });
   }
 });
