200f2b915e6a627473ae5d9e85f9638a07a53962
emy-wins <benkhadra.iman@gmail.com>
2024-11-13 13:52:44 +0100

    #update

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -30,3 +30,50 @@ $(document).ready(function ($) {
     });
   }
 });
+
+
+$(document).ready(function ($) {
+  $("#form-login").validate({
+    rules: {
+      cnie: "required",
+      password: {
+        required: true,
+        minlength: 6,
+        maxlength: 10,
+      },
+    },
+    messages: {
+      cnie: "CNIE est obligatoire",
+      password: {
+        required: "Mot de passe est obligatoire",
+        minlength: "Le mot de passe doit contenir au minimum 6 caractères.",
+        maxlength: "Le mot de passe ne doit pas dépasser 10 caractères"
+      },
+    },
+    errorPlacement: function (error, element) {
+      error.insertAfter(element);
+    },
+    submitHandler: function (form) {
+      form.submit();
+    }
+
+  });
+  $("#form-verifier").validate({
+    rules: {
+      cnie: "required",
+      email: "required",
+      date: "required",
+    },
+    messages: {
+      cnie: "CNIE est obligatoire",
+      email: "email est obligatoire",
+      date: "Date naissance est obligatoire",
+    },
+    errorPlacement: function (error, element) {
+      error.insertAfter(element);
+    },
+    submitHandler: function (form) {
+      form.submit();
+    }
+  });
+});
